f08700d48621b795b67c4a99ec2af8ea301bea5b
longwanxiang <wanxiang.long@wukongshc.com>
2021-12-28 14:16:33 +0800

    chore: use github workflow

---
+++ scripts/robotstxt.js
@@ -0,0 +1,18 @@
+hexo.extend.generator.register('robotstxt', function () {
+  return {
+    path: 'robots.txt',
+    data: function () {
+      let body = '';
+      const isVercel = process.env.VERCEL_ENV === 'production';
+      if (isVercel) {
+        body = 'User-agent: Googlebot\nDisallow: /';
+      } else {
+        body = 'User-agent: *\nAllow: /';
+      }
+
+      body += `\n\nSitemap: https://${isVercel ? 'ryuurock.vercel.app' : 'ryuurock.github.io'}/sitemap.xml`
+
+      return body;
+    }
+  };
+});
